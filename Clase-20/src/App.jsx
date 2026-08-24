8e0955dbfc6dd932b68bb30e0c05aeb4497280b8
jere.rar <lfjeremias@hotmail.com>
2026-08-24 11:58:10 -0300

    Clase 24/8

---
+++ Clase-20/src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import loquevenga from "./math"
+
+console.log(loquevenga(60, 40))
+
+export default function App() {
+  return (
+    <div>Hola, esta es mi primera app en React!</div>
+  )
+}
---
+++ Clase-20/src/math.js
@@ -0,0 +1,13 @@
+export const PI = 3.14
+
+function sumar(a, b){
+    return a + b
+}
+
+const restar = (a, b) => {
+    return a - b
+}
+
+export{sumar, restar}
+
+export default sumar
